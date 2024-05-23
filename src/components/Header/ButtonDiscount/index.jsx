593989562a13244d6c24e2b3f4346531a85d8d29
ElenaK <36508011+Ferst1@users.noreply.github.com>
2024-05-23 18:27:11 +0200

    updated
    добавила,стелизовала кнопку в навигацию

---
+++ src/components/Header/ButtonDiscount/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import s from './ButtonDiscount.module.css'
+const index = () => {
+  return (
+    <div>
+        <button className={s.buttonDiscount}>1 day discount!</button>
+    </div>
+  )
+}
+
+export default index
--- src/components/Header/Navigation/index.jsx
+++ src/components/Header/Navigation/index.jsx
@@ -1,9 +1,13 @@
 import { NavLink } from "react-router-dom";
 import s from "../Navigation/Navigation.module.css";
+import ButtonDiscount from "../ButtonDiscount";
 
 const Navigation = () => {
   return (
+    <div className={s.navigation}>
+      <ButtonDiscount />
     <nav className={s.mainNavLink}>
+      
       <NavLink to={"/main_page"}>Main Page</NavLink>
 
       <NavLink to={"/categories"}>Categories</NavLink>
@@ -14,6 +18,7 @@ const Navigation = () => {
         All sales
       </NavLink>
     </nav>
+    </div>
   );
 };
 
