0f2af57f1689b8a21d1a6537a6c67dfe65b24545
ElenaK <36508011+Ferst1@users.noreply.github.com>
2024-05-23 20:35:01 +0200

    updated header
    позиционирование навигации

---
+++ src/components/Header/Switcher/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>Switcher</div>
+  )
+}
+
+export default index
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import Navigation from './Navigation'
-import s from './Header.module.css'
+import React from "react";
+import Navigation from "./Navigation";
+import s from "./Header.module.css";
+import Logo from "./Logo";
+import Switcher from "./Switcher";
+import Favorite from "./Favorite";
+import Basket from "./Basket";
 const Header = () => {
   return (
     <div className={s.header}>
+      <Logo />
+      <Switcher />
       <Navigation />
+      <Favorite />
+      <Basket />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
