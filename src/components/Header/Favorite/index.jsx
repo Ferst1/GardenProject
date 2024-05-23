5966fab904559847273d55785973bf346fb8d52d
elviraghibner <elvira.ghibner@gmail.com>
2024-05-24 00:39:29 +0200

    new header

--- src/components/Header/Favorite/index.jsx
+++ src/components/Header/Favorite/index.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import FavDark from "../../../media/icons/favorite-night-icon.svg";
+import FavLight from '../../../media/icons/favorite-light-icon.svg'
 
-const index = () => {
+
+const Favorite = ({ isDarkMode }) => {
   return (
-    <div>Favorite</div>
-  )
-}
+    <div>
+      {isDarkMode ? (
+        <img src={FavLight} alt="Favorite Icon"/>
+      ) : (
+        <img src={FavDark} alt="Favorite Icon" />
+      )}
+    </div>
+  );
+};
 
-export default index
+export default Favorite;
