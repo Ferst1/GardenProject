9b2801132eb4d79bfb02e799f16e3614a7fad212
ElenaK <36508011+Ferst1@users.noreply.github.com>
2024-05-24 22:20:28 +0200

    mobile-menu
    mobile-menu adaptive

--- src/components/Header/BurgerMenu/index.jsx
+++ src/components/Header/BurgerMenu/index.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import burgerMenuSVG from '../../../media/icons/burger-menu.svg'
-import s from './BurgerMenu.module.css'
-const BurgerMenu
- = () => {
-  return (
-    <div className={s.burger_menu}>
-      <img src={burgerMenuSVG} alt="Burger Menu Icon" />
-        
-    </div>
-  )
-}
 
-export default BurgerMenu
+
+
+import React from "react";
+import burgerMenuSVG from "../../../media/icons/burger-menu.svg";
+import s from "./BurgerMenu.module.css";
+
+const BurgerMenu = ({ onClick }) => {
+  const handleClick = () => {
+    console.log("Clicked on BurgerMenu");
+    onClick();
+  };
+
+  return (
+    <div className={s.burger_menu} onClick={handleClick}>
+      <img src={burgerMenuSVG} alt="Burger Menu Icon" />
+    </div>
+  );
+};
+
+export default BurgerMenu;
