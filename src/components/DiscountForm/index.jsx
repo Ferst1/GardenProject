010c0ab84a9111b72f3a6d03db99339e1f429dd5
elviraghibner <elvira.ghibner@gmail.com>
2024-05-25 08:32:02 +0200

    new_3

--- src/components/DiscountForm/index.jsx
+++ src/components/DiscountForm/index.jsx
@@ -5,8 +5,9 @@ import HandsImage from '../../media/images/hands-image.svg'
 export default function DiscountForm() {
 
   return (
+    <div className={s.form_section}>
       <div className={s.form_container}>
-        <img src={HandsImage} alt="Hands holding garden tools" className={s.form_image} />
+      <img src={HandsImage} alt="Hands holding garden tools" className={s.form_image} />
         <h1>5% off on the first order</h1>
         <form>
           <input type="text" name="name" placeholder="Name" required />
@@ -14,6 +15,7 @@ export default function DiscountForm() {
           <input type="email" name="email" placeholder="Email" required />
           <button type="submit">Get a discount</button>
         </form>
+      </div>
       </div>
   )
 }
