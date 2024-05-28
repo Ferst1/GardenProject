fcab8110ba4dd5603b778269b3ed69a51f6a7320
Ferst1 <36508011+Ferst1@users.noreply.github.com>
2024-05-28 21:55:03 +0200

    modal-window

--- src/components/Header/ButtonDiscount/index.jsx
+++ src/components/Header/ButtonDiscount/index.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import s from './ButtonDiscount.module.css'
-const index = () => {
+
+import React, { useRef } from 'react';
+import s from './ButtonDiscount.module.css';
+import SaleModal from '../../Hero/SaleModal';
+
+const ButtonDiscount = () => {
+  const saleModalRef = useRef();
+
+  const handleOpen = () => {
+    if (saleModalRef.current) {
+      saleModalRef.current.openModal();
+    }
+  };
+
   return (
     <div>
-        <button className={s.buttonDiscount}>1 day discount!</button>
+      <button className={s.buttonDiscount} onClick={handleOpen}>
+        1 day discount!
+      </button>
+      <SaleModal ref={saleModalRef} />
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default ButtonDiscount;
+
