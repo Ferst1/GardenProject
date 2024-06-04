fcf55075d8610633342d6dfbaa12ba1ab08c6385
Ferst1 <36508011+Ferst1@users.noreply.github.com>
2024-06-04 14:17:56 +0200

    fetch allProducts
    fetch All products

--- src/components/AllProducts/index.jsx
+++ src/components/AllProducts/index.jsx
@@ -11,23 +11,24 @@ const AllProducts = ({ products }) => {
     <div>
       {products.map((product) => (
         <div key={product.id} className={styles.product_item}>
-          {product.image ? (
-            <div className={styles.category_content}>
+          <div className={styles.category_content}>
+            {product.image && (
               <img
                 className={styles.category_img}
                 src={`${baseUrl}${product.image}`}
                 alt={product.title}
               />
-              <div className={styles.product_title}>{product.title}</div>
+            )}
+            <div className={styles.product_title}>{product.title}</div>
+            <div className={styles.product_price}>
+              ${product.price}
+              {product.discont_price && (
+                <span className={styles.discont_price}>
+                  ${product.discont_price}
+                </span>
+              )}
             </div>
-          ) : (
-            <div className={styles.category_content}>
-              <div className={styles.product_title}>{product.title}</div>
-              
-            </div>
-          )}
-       
-
+          </div>
         </div>
       ))}
     </div>
