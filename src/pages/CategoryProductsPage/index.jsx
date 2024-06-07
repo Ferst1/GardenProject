0973a1a0429eab4bf5ec09b3190f08b369f3b725
Ferst1 <36508011+Ferst1@users.noreply.github.com>
2024-06-07 21:37:50 +0200

    procent from discount
    procent from discount

--- src/pages/CategoryProductsPage/index.jsx
+++ src/pages/CategoryProductsPage/index.jsx
@@ -1,4 +1,5 @@
 
+
 import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -55,35 +56,43 @@ const CategoryProductsPage = () => {
           <SortFilteredComponents />
         </div>
         <div className={s.products_grid}>
-          {products.map((product) => (
-            <div key={product.id} className={s.product_item}>
-              <div className={s.category_content}>
-                <div className={s.cards_button}>
-                  <Basket />
-                  <Favorite />
-                  {product.image && (
-                    <img
-                      className={s.category_img}
-                      src={`${baseUrl}${product.image}`}
-                      alt={product.title}
-                    />
-                  )}
-                  {product.discount && (
-                    <div className={s.discont_tag}>{`-${product.discount}%`}</div>
-                  )}
-                  <div className={s.product_title}>{product.title}</div>
-                  <div className={s.product_price}>
-                    ${product.price}
-                    {product.discont_price && (
-                      <span className={s.discont_price}>
-                        ${product.discont_price}
-                      </span>
+          {products.map((product) => {
+          
+            let discont = null;
+            if (product.price && product.discont_price) {
+              discont = Math.round(((product.price - product.discont_price) / product.price) * 100);
+            }
+
+            return (
+              <div key={product.id} className={s.product_item}>
+                <div className={s.category_content}>
+                  <div className={s.cards_button}>
+                    <Basket />
+                    <Favorite />
+                    {product.image && (
+                      <img
+                        className={s.category_img}
+                        src={`${baseUrl}${product.image}`}
+                        alt={product.title}
+                      />
                     )}
+                    {discont !== null && (
+                      <div className={s.discont_tag}>{`-${discont}%`}</div>
+                    )}
+                    <div className={s.product_title}>{product.title}</div>
+                    <div className={s.product_price}>
+                      ${product.price}
+                      {product.discont_price && (
+                        <span className={s.discont_price}>
+                          ${product.discont_price}
+                        </span>
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </div>
