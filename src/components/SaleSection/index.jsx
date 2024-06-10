6369f7144a31b138ba267e3b067769db21655448
Ferst1 <36508011+Ferst1@users.noreply.github.com>
2024-06-10 20:11:07 +0200

    sale-section
    sale-section ready

--- src/components/SaleSection/index.jsx
+++ src/components/SaleSection/index.jsx
@@ -1,10 +1,31 @@
-
-import React from "react"; 
-import SaleCards from "../SaleCards";
- import ButtonSection from "../UI/ButtonSection";
-import s from "./SaleSection.module.css";
+import React, { useEffect } from 'react'; 
+import { useDispatch, useSelector } from 'react-redux';
+import SaleCards from '../SaleCards';
+import ButtonSection from '../UI/ButtonSection';
+import { fetchProducts } from '../../redux/actions/productsActions';
+import s from './SaleSection.module.css';
 
 const SaleSection = () => {
+  const dispatch = useDispatch();
+  const { products, loading, error } = useSelector(state => state.products);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  const saleProducts = products
+    .filter(product => product.discont_price)
+    .sort((a, b) => a.discont_price - b.discont_price)
+    .slice(0, 4);
+
   return (
     <div>
       <section>
@@ -14,7 +35,7 @@ const SaleSection = () => {
             <ButtonSection text="All sales" />
           </div>
         </div>
-        <SaleCards />
+        <SaleCards products={saleProducts} />
       </section>
     </div>
   );
