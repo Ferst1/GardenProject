e102f319b7d1bf31c78f2f90364bc5bef79a8468
Ferst1 <36508011+Ferst1@users.noreply.github.com>
2024-06-13 18:24:43 +0200

    Products Card
    карточка открывается в деталях,но при клике на корзину тоже идет событие.ошибок нет

--- src/components/ProductsCard/index.jsx
+++ src/components/ProductsCard/index.jsx
@@ -1,195 +1,5 @@
 
-// import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
-// import baseUrl from "../../instance";
-// import s from "./ProductsCard.module.css";
-// import ButtonAddToCard from "../UI/ButtonAddToCard";
-// import Favorite from "../../components/Header/Favorite";
-// import Basket from "../../components/Header/Basket";
-// import { useDispatch, useSelector } from 'react-redux';
-// import { addToBasket } from '../../redux/basketReducer';
-// import { addToFavorites, removeFromFavorites } from '../../redux/actions/productsActions';
 
-// const ProductsCard = ({ product, showAddToCartButton }) => {
-//   const dispatch = useDispatch();
-//   const basket = useSelector((state) => state.basket.basket);
-//   const favorites = useSelector((state) => state.products.favorites);
-//   const [isInBasket, setIsInBasket] = useState(false);
-//   const [isFavorite, setIsFavorite] = useState(false);
-
-//   useEffect(() => {
-//     setIsInBasket(basket.some(item => item.id === product.id));
-//   }, [basket, product.id]);
-
-//   useEffect(() => {
-//     setIsFavorite(favorites.some(fav => fav.id === product.id));
-//   }, [favorites, product.id]);
-
-//   if (!product) {
-//     return <div>Product data is missing</div>;
-//   }
-
-//   let discount = null;
-//   if (product.price && product.discont_price) {
-//     discount = Math.round(
-//       ((product.price - product.discont_price) / product.price) * 100
-//     );
-//   }
-
-//   const handleAddToBasket = (e) => {
-//     e.preventDefault();
-//     dispatch(addToBasket(product));
-//   };
-
-//   const handleAddToFavorites = (e) => {
-//     e.preventDefault();
-//     if (!isFavorite) {
-//       dispatch(addToFavorites(product));
-//     } else {
-//       dispatch(removeFromFavorites(product));
-//     }
-//     setIsFavorite(!isFavorite);
-//   };
-
-//   return (
-//     <div className={s.product_item}>
-//       <Link to={`/product/${product.id}`}>
-//         <div className={s.category_content}>
-//           <div className={s.image_container}>
-//             {product.image && (
-//               <>
-//                 <img
-//                   className={s.category_img}
-//                   src={`${baseUrl}${product.image}`}
-//                   alt={product.title}
-//                 />
-//                 <div className={s.icons}>
-//                   <Favorite onClick={handleAddToFavorites} isFavorite={isFavorite}/>
-//                   <Basket onClick={handleAddToBasket} isInBasket={isInBasket} />
-//                 </div>
-//                 {showAddToCartButton && (
-//                   <ButtonAddToCard text="Add to Cart" onClick={handleAddToBasket} />
-//                 )}
-//               </>
-//             )}
-//             {discount !== null && (
-//               <div className={s.discont_tag}>{`-${discount}%`}</div>
-//             )}
-//           </div>
-//           <div className={s.product_title}>{product.title}</div>
-//           <div className={s.product_price}>
-//             ${product.price}
-//             {product.discont_price && (
-//               <span className={s.discont_price}>${product.discont_price}</span>
-//             )}
-//           </div>
-//         </div>
-//       </Link>
-//     </div>
-//   );
-// };
-
-// export default ProductsCard;
-
-
-
-
-// import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
-// import baseUrl from "../../instance";
-// import s from "./ProductsCard.module.css";
-// import ButtonAddToCard from "../UI/ButtonAddToCard";
-// import Favorite from "../../components/Header/Favorite";
-// import Basket from "../../components/Header/Basket";
-// import { useDispatch, useSelector } from 'react-redux';
-// import { addToBasket, removeFromBasket } from '../../redux/basketReducer'; // добавляем removeFromBasket
-// import { addToFavorites, removeFromFavorites } from '../../redux/actions/productsActions';
-
-// const ProductsCard = ({ product, showAddToCartButton }) => {
-//   const dispatch = useDispatch();
-//   const basket = useSelector((state) => state.basket.basket);
-//   const favorites = useSelector((state) => state.products.favorites);
-//   const [isInBasket, setIsInBasket] = useState(false);
-//   const [isFavorite, setIsFavorite] = useState(false);
-
-//   useEffect(() => {
-//     setIsInBasket(basket.some(item => item.id === product.id));
-//   }, [basket, product.id]);
-
-//   useEffect(() => {
-//     setIsFavorite(favorites.some(fav => fav.id === product.id));
-//   }, [favorites, product.id]);
-
-//   if (!product) {
-//     return <div>Product data is missing</div>;
-//   }
-
-//   let discount = null;
-//   if (product.price && product.discont_price) {
-//     discount = Math.round(
-//       ((product.price - product.discont_price) / product.price) * 100
-//     );
-//   }
-
-//   const handleAddToBasket = (e) => {
-//     e.preventDefault();
-//     if (isInBasket) {
-//       dispatch(removeFromBasket(product.id));
-//     } else {
-//       dispatch(addToBasket(product));
-//     }
-//     setIsInBasket(!isInBasket);
-//   };
-
-//   const handleAddToFavorites = (e) => {
-//     e.preventDefault();
-//     if (!isFavorite) {
-//       dispatch(addToFavorites(product));
-//     } else {
-//       dispatch(removeFromFavorites(product));
-//     }
-//     setIsFavorite(!isFavorite);
-//   };
-
-//   return (
-//     <div className={s.product_item}>
-//       <Link to={`/product/${product.id}`}>
-//         <div className={s.category_content}>
-//           <div className={s.image_container}>
-//             {product.image && (
-//               <>
-//                 <img
-//                   className={s.category_img}
-//                   src={`${baseUrl}${product.image}`}
-//                   alt={product.title}
-//                 />
-//                 <div className={s.icons}>
-//                   <Favorite onClick={handleAddToFavorites} isFavorite={isFavorite}/>
-//                   <Basket onClick={handleAddToBasket} isInBasket={isInBasket} />
-//                 </div>
-//                 {showAddToCartButton && (
-//                   <ButtonAddToCard text="Add to Cart" onClick={handleAddToBasket} />
-//                 )}
-//               </>
-//             )}
-//             {discount !== null && (
-//               <div className={s.discont_tag}>{`-${discount}%`}</div>
-//             )}
-//           </div>
-//           <div className={s.product_title}>{product.title}</div>
-//           <div className={s.product_price}>
-//             ${product.price}
-//             {product.discont_price && (
-//               <span className={s.discont_price}>${product.discont_price}</span>
-//             )}
-//           </div>
-//         </div>
-//       </Link>
-//     </div>
-//   );
-// };
-
-// export default ProductsCard;
 
 // import React, { useState, useEffect } from "react";
 // import { Link } from "react-router-dom";
@@ -228,7 +38,8 @@
 //     );
 //   }
 
-//   const handleAddToBasket = (addToBasketAction) => {
+//   const handleAddToBasket = (e, addToBasketAction) => {
+//     e.preventDefault(); // предотвратить переход по ссылке
 //     if (addToBasketAction) {
 //       dispatch(addToBasket(product));
 //     } else {
@@ -261,10 +72,10 @@
 //                 />
 //                 <div className={s.icons}>
 //                   <Favorite onClick={handleAddToFavorites} isFavorite={isFavorite} />
-//                   <Basket onClick={handleAddToBasket} isInBasket={isInBasket} />
+//                   <Basket onClick={(e) => handleAddToBasket(e, !isInBasket)} isInBasket={isInBasket} />
 //                 </div>
 //                 {showAddToCartButton && (
-//                   <ButtonAddToCard text="Add to Cart" onClick={() => handleAddToBasket(true)} />
+//                   <ButtonAddToCard text="Add to Cart" onClick={(e) => handleAddToBasket(e, true)} />
 //                 )}
 //               </>
 //             )}
@@ -287,6 +98,206 @@
 
 // export default ProductsCard;
 
+
+
+// import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+// import baseUrl from "../../instance";
+// import s from "./ProductsCard.module.css";
+// import ButtonAddToCard from "../UI/ButtonAddToCard";
+// import Favorite from "../../components/Header/Favorite";
+// import Basket from "../../components/Header/Basket";
+// import { useDispatch, useSelector } from 'react-redux';
+// import { addToBasket, removeFromBasket } from '../../redux/basketReducer';
+// import { addToFavorites, removeFromFavorites } from '../../redux/actions/productsActions';
+
+// const ProductsCard = ({ product, showAddToCartButton }) => {
+//   const dispatch = useDispatch();
+//   const basket = useSelector((state) => state.basket.basket);
+//   const favorites = useSelector((state) => state.products.favorites);
+//   const [isInBasket, setIsInBasket] = useState(false);
+//   const [isFavorite, setIsFavorite] = useState(false);
+
+//   useEffect(() => {
+//     setIsInBasket(basket.some(item => item.id === product.id));
+//   }, [basket, product.id]);
+
+//   useEffect(() => {
+//     setIsFavorite(favorites.some(fav => fav.id === product.id));
+//   }, [favorites, product.id]);
+
+//   if (!product) {
+//     return <div>Product data is missing</div>;
+//   }
+
+//   let discount = null;
+//   if (product.price && product.discont_price) {
+//     discount = Math.round(
+//       ((product.price - product.discont_price) / product.price) * 100
+//     );
+//   }
+
+//   const handleAddToBasket = (e, addToBasketAction) => {
+//     if (e && e.preventDefault) {
+//       e.preventDefault(); // предотвратить переход по ссылке
+//     }
+//     if (addToBasketAction) {
+//       dispatch(addToBasket(product));
+//     } else {
+//       dispatch(removeFromBasket(product.id));
+//     }
+//     setIsInBasket(addToBasketAction);
+//   };
+
+//   const handleAddToFavorites = (e) => {
+//     e.preventDefault();
+//     if (!isFavorite) {
+//       dispatch(addToFavorites(product));
+//     } else {
+//       dispatch(removeFromFavorites(product));
+//     }
+//     setIsFavorite(!isFavorite);
+//   };
+
+//   return (
+//     <div className={s.product_item}>
+//       <Link to={`/product/${product.id}`}>
+//         <div className={s.category_content}>
+//           <div className={s.image_container}>
+//             {product.image && (
+//               <>
+//                 <img
+//                   className={s.category_img}
+//                   src={`${baseUrl}${product.image}`}
+//                   alt={product.title}
+//                 />
+//                 <div className={s.icons}>
+//                   <Favorite onClick={handleAddToFavorites} isFavorite={isFavorite} />
+//                   <Basket onClick={(e) => handleAddToBasket(e, !isInBasket)} isInBasket={isInBasket} />
+//                 </div>
+//                 {showAddToCartButton && (
+//                   <ButtonAddToCard text="Add to Cart" onClick={(e) => handleAddToBasket(e, true)} />
+//                 )}
+//               </>
+//             )}
+//             {discount !== null && (
+//               <div className={s.discont_tag}>{`-${discount}%`}</div>
+//             )}
+//           </div>
+//           <div className={s.product_title}>{product.title}</div>
+//           <div className={s.product_price}>
+//             ${product.price}
+//             {product.discont_price && (
+//               <span className={s.discont_price}>${product.discont_price}</span>
+//             )}
+//           </div>
+//         </div>
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default ProductsCard;
+
+
+// import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+// import baseUrl from "../../instance";
+// import s from "./ProductsCard.module.css";
+// import ButtonAddToCard from "../UI/ButtonAddToCard";
+// import Favorite from "../../components/Header/Favorite";
+// import Basket from "../../components/Header/Basket";
+// import { useDispatch, useSelector } from 'react-redux';
+// import { addToBasket, removeFromBasket } from '../../redux/basketReducer';
+// import { addToFavorites, removeFromFavorites } from '../../redux/actions/productsActions';
+
+// const ProductsCard = ({ product, showAddToCartButton }) => {
+//   const dispatch = useDispatch();
+//   const basket = useSelector((state) => state.basket.basket);
+//   const favorites = useSelector((state) => state.products.favorites);
+//   const [isInBasket, setIsInBasket] = useState(false);
+//   const [isFavorite, setIsFavorite] = useState(false);
+
+//   useEffect(() => {
+//     setIsInBasket(basket.some(item => item.id === product.id));
+//   }, [basket, product.id]);
+
+//   useEffect(() => {
+//     setIsFavorite(favorites.some(fav => fav.id === product.id));
+//   }, [favorites, product.id]);
+
+//   if (!product) {
+//     return <div>Product data is missing</div>;
+//   }
+
+//   let discount = null;
+//   if (product.price && product.discont_price) {
+//     discount = Math.round(
+//       ((product.price - product.discont_price) / product.price) * 100
+//     );
+//   }
+
+//   const handleAddToBasket = (e, addToBasketAction) => {
+//     e.preventDefault();
+//     e.stopPropagation(); // остановить распространение события
+//     if (addToBasketAction) {
+//       dispatch(addToBasket(product));
+//     } else {
+//       dispatch(removeFromBasket(product.id));
+//     }
+//     setIsInBasket(addToBasketAction);
+//   };
+
+//   const handleAddToFavorites = (e) => {
+//     e.preventDefault();
+//     e.stopPropagation(); // остановить распространение события
+//     if (!isFavorite) {
+//       dispatch(addToFavorites(product));
+//     } else {
+//       dispatch(removeFromFavorites(product));
+//     }
+//     setIsFavorite(!isFavorite);
+//   };
+
+//   return (
+//     <div className={s.product_item}>
+//       <Link to={`/product/${product.id}`}>
+//         <div className={s.category_content}>
+//           <div className={s.image_container}>
+//             {product.image && (
+//               <>
+//                 <img
+//                   className={s.category_img}
+//                   src={`${baseUrl}${product.image}`}
+//                   alt={product.title}
+//                 />
+//                 <div className={s.icons}>
+//                   <Favorite onClick={handleAddToFavorites} isFavorite={isFavorite} />
+//                   <Basket onClick={(e) => handleAddToBasket(e, !isInBasket)} isInBasket={isInBasket} />
+//                 </div>
+//                 {showAddToCartButton && (
+//                   <ButtonAddToCard text="Add to Cart" onClick={(e) => handleAddToBasket(e, true)} />
+//                 )}
+//               </>
+//             )}
+//             {discount !== null && (
+//               <div className={s.discont_tag}>{`-${discount}%`}</div>
+//             )}
+//           </div>
+//           <div className={s.product_title}>{product.title}</div>
+//           <div className={s.product_price}>
+//             ${product.price}
+//             {product.discont_price && (
+//               <span className={s.discont_price}>${product.discont_price}</span>
+//             )}
+//           </div>
+//         </div>
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default ProductsCard;
 
 
 import React, { useState, useEffect } from "react";
@@ -327,7 +338,10 @@ const ProductsCard = ({ product, showAddToCartButton }) => {
   }
 
   const handleAddToBasket = (e, addToBasketAction) => {
-    e.preventDefault(); // предотвратить переход по ссылке
+    if (e && typeof e.preventDefault === 'function') {
+      e.preventDefault();
+      e.stopPropagation();
+    }
     if (addToBasketAction) {
       dispatch(addToBasket(product));
     } else {
@@ -337,7 +351,10 @@ const ProductsCard = ({ product, showAddToCartButton }) => {
   };
 
   const handleAddToFavorites = (e) => {
-    e.preventDefault();
+    if (e && typeof e.preventDefault === 'function') {
+      e.preventDefault();
+      e.stopPropagation();
+    }
     if (!isFavorite) {
       dispatch(addToFavorites(product));
     } else {
@@ -385,4 +402,3 @@ const ProductsCard = ({ product, showAddToCartButton }) => {
 };
 
 export default ProductsCard;
-
