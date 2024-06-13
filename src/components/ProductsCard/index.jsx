cd26e8512b8a56c8ed8f45d22792cc7774f87cff
Ferst1 <36508011+Ferst1@users.noreply.github.com>
2024-06-13 18:08:08 +0200

    updated all

--- src/components/ProductsCard/index.jsx
+++ src/components/ProductsCard/index.jsx
@@ -289,201 +289,6 @@
 
 
 
-// import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
-// import baseUrl from "../../instance";
-// import s from "./ProductsCard.module.css";
-// import ButtonAddToCard from "../UI/ButtonAddToCard";
-// import Favorite from "../../components/Header/Favorite";
-// import Basket from "../../components/Header/Basket";
-// import { useDispatch, useSelector } from 'react-redux';
-// import { addToBasket, removeFromBasket } from '../../redux/basketReducer';
-// import { addToFavorites, removeFromFavorites } from '../../redux/actions/productsActions';
-
-// const ProductsCard = ({ product, showAddToCartButton }) => {
-//   const dispatch = useDispatch();
-//   const basket = useSelector((state) => state.basket.basket);
-//   const favorites = useSelector((state) => state.products.favorites);
-//   const [isInBasket, setIsInBasket] = useState(false);
-//   const [isFavorite, setIsFavorite] = useState(false);
-
-//   useEffect(() => {
-//     setIsInBasket(basket.some(item => item.id === product.id));
-//   }, [basket, product.id]);
-
-//   useEffect(() => {
-//     setIsFavorite(favorites.some(fav => fav.id === product.id));
-//   }, [favorites, product.id]);
-
-//   if (!product) {
-//     return <div>Product data is missing</div>;
-//   }
-
-//   let discount = null;
-//   if (product.price && product.discont_price) {
-//     discount = Math.round(
-//       ((product.price - product.discont_price) / product.price) * 100
-//     );
-//   }
-
-//   const handleAddToBasket = (e, addToBasketAction) => {
-//     e.preventDefault(); // предотвратить переход по ссылке
-//     if (addToBasketAction) {
-//       dispatch(addToBasket(product));
-//     } else {
-//       dispatch(removeFromBasket(product.id));
-//     }
-//     setIsInBasket(addToBasketAction);
-//   };
-
-//   const handleAddToFavorites = (e) => {
-//     e.preventDefault();
-//     if (!isFavorite) {
-//       dispatch(addToFavorites(product));
-//     } else {
-//       dispatch(removeFromFavorites(product));
-//     }
-//     setIsFavorite(!isFavorite);
-//   };
-
-//   return (
-//     <div className={s.product_item}>
-//       <Link to={`/product/${product.id}`}>
-//         <div className={s.category_content}>
-//           <div className={s.image_container}>
-//             {product.image && (
-//               <>
-//                 <img
-//                   className={s.category_img}
-//                   src={`${baseUrl}${product.image}`}
-//                   alt={product.title}
-//                 />
-//                 <div className={s.icons}>
-//                   <Favorite onClick={handleAddToFavorites} isFavorite={isFavorite} />
-//                   <Basket onClick={(e) => handleAddToBasket(e, !isInBasket)} isInBasket={isInBasket} />
-//                 </div>
-//                 {showAddToCartButton && (
-//                   <ButtonAddToCard text="Add to Cart" onClick={(e) => handleAddToBasket(e, true)} />
-//                 )}
-//               </>
-//             )}
-//             {discount !== null && (
-//               <div className={s.discont_tag}>{`-${discount}%`}</div>
-//             )}
-//           </div>
-//           <div className={s.product_title}>{product.title}</div>
-//           <div className={s.product_price}>
-//             ${product.price}
-//             {product.discont_price && (
-//               <span className={s.discont_price}>${product.discont_price}</span>
-//             )}
-//           </div>
-//         </div>
-//       </Link>
-//     </div>
-//   );
-// };
-
-// export default ProductsCard;
-
-// import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
-// import baseUrl from "../../instance";
-// import s from "./ProductsCard.module.css";
-// import ButtonAddToCard from "../UI/ButtonAddToCard";
-// import Favorite from "../../components/Header/Favorite";
-// import Basket from "../../components/Header/Basket";
-// import { useDispatch, useSelector } from 'react-redux';
-// import { addToBasket, removeFromBasket } from '../../redux/basketReducer';
-// import { addToFavorites, removeFromFavorites } from '../../redux/actions/productsActions';
-
-// const ProductsCard = ({ product, showAddToCartButton }) => {
-//   const dispatch = useDispatch();
-//   const basket = useSelector((state) => state.basket.basket);
-//   const favorites = useSelector((state) => state.products.favorites);
-//   const [isInBasket, setIsInBasket] = useState(false);
-//   const [isFavorite, setIsFavorite] = useState(false);
-
-//   useEffect(() => {
-//     setIsInBasket(basket.some(item => item.id === product.id));
-//   }, [basket, product.id]);
-
-//   useEffect(() => {
-//     setIsFavorite(favorites.some(fav => fav.id === product.id));
-//   }, [favorites, product.id]);
-
-//   if (!product) {
-//     return <div>Product data is missing</div>;
-//   }
-
-//   let discount = null;
-//   if (product.price && product.discont_price) {
-//     discount = Math.round(
-//       ((product.price - product.discont_price) / product.price) * 100
-//     );
-//   }
-
-//   const handleAddToBasket = (addToBasketAction) => {
-//     if (addToBasketAction) {
-//       dispatch(addToBasket(product));
-//     } else {
-//       dispatch(removeFromBasket(product.id));
-//     }
-//     setIsInBasket(addToBasketAction);
-//   };
-
-//   const handleAddToFavorites = (e) => {
-//     e.preventDefault();
-//     if (!isFavorite) {
-//       dispatch(addToFavorites(product));
-//     } else {
-//       dispatch(removeFromFavorites(product));
-//     }
-//     setIsFavorite(!isFavorite);
-//   };
-
-//   return (
-//     <div className={s.product_item}>
-//       <Link to={`/product/${product.id}`} onClick={(e) => e.preventDefault()}>
-//         <div className={s.category_content}>
-//           <div className={s.image_container}>
-//             {product.image && (
-//               <>
-//                 <img
-//                   className={s.category_img}
-//                   src={`${baseUrl}${product.image}`}
-//                   alt={product.title}
-//                 />
-//                 <div className={s.icons}>
-//                   <Favorite onClick={handleAddToFavorites} isFavorite={isFavorite} />
-//                   <Basket onClick={handleAddToBasket} isInBasket={isInBasket} />
-//                 </div>
-//                 {showAddToCartButton && (
-//                   <ButtonAddToCard text="Add to Cart" onClick={() => handleAddToBasket(true)} />
-//                 )}
-//               </>
-//             )}
-//             {discount !== null && (
-//               <div className={s.discont_tag}>{`-${discount}%`}</div>
-//             )}
-//           </div>
-//           <div className={s.product_title}>{product.title}</div>
-//           <div className={s.product_price}>
-//             ${product.price}
-//             {product.discont_price && (
-//               <span className={s.discont_price}>${product.discont_price}</span>
-//             )}
-//           </div>
-//         </div>
-//       </Link>
-//     </div>
-//   );
-// };
-
-// export default ProductsCard;
-
-
-
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import baseUrl from "../../instance";
@@ -521,7 +326,8 @@ const ProductsCard = ({ product, showAddToCartButton }) => {
     );
   }
 
-  const handleAddToBasket = (addToBasketAction) => {
+  const handleAddToBasket = (e, addToBasketAction) => {
+    e.preventDefault(); // предотвратить переход по ссылке
     if (addToBasketAction) {
       dispatch(addToBasket(product));
     } else {
@@ -542,7 +348,7 @@ const ProductsCard = ({ product, showAddToCartButton }) => {
 
   return (
     <div className={s.product_item}>
-      <Link to={`/product/${product.id}`} onClick={(e) => e.preventDefault()}>
+      <Link to={`/product/${product.id}`}>
         <div className={s.category_content}>
           <div className={s.image_container}>
             {product.image && (
@@ -554,10 +360,10 @@ const ProductsCard = ({ product, showAddToCartButton }) => {
                 />
                 <div className={s.icons}>
                   <Favorite onClick={handleAddToFavorites} isFavorite={isFavorite} />
-                  <Basket onClick={handleAddToBasket} isInBasket={isInBasket} />
+                  <Basket onClick={(e) => handleAddToBasket(e, !isInBasket)} isInBasket={isInBasket} />
                 </div>
                 {showAddToCartButton && (
-                  <ButtonAddToCard text="Add to Cart" onClick={() => handleAddToBasket(true)} />
+                  <ButtonAddToCard text="Add to Cart" onClick={(e) => handleAddToBasket(e, true)} />
                 )}
               </>
             )}
@@ -579,3 +385,4 @@ const ProductsCard = ({ product, showAddToCartButton }) => {
 };
 
 export default ProductsCard;
+
--- src/pages/CategoryProductsPage/index.jsx
+++ src/pages/CategoryProductsPage/index.jsx
@@ -1,8 +1,86 @@
 
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
+// import { Link, useParams } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchProductsByCategory } from "../../redux/actions/productsActions";
+// import styles from "./CategoryProductsPage.module.css";
+// import SortFilteredComponents from "../../components/SortFilteredComponents";
+// import ButtonSection from "../../components/UI/ButtonSection";
+// import CategoryProductsCard from "../../components/CategoryProductsCard";
+
+// const CategoryProductsPage = () => {
+//   const { categoryId } = useParams();
+//   const dispatch = useDispatch();
+//   const { products, loading, error, filters } = useSelector(
+//     (state) => state.products
+//   );
+
+//   useEffect(() => {
+//     dispatch(fetchProductsByCategory(categoryId));
+//   }, [dispatch, categoryId]);
+
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (error) {
+//     return <div>Error: {error}</div>;
+//   }
+
+//   if (!Array.isArray(products) || products.length === 0) {
+//     return <div>No products available</div>;
+//   }
+
+//   const filteredProducts = products.filter((product) => {
+//     return (
+//       product.price >= filters.minPrice && product.price <= filters.maxPrice
+//     );
+//   });
+
+//   return (
+//     <div className="container">
+//       <div>
+//         <div className={styles.buttons_wrapper}>
+//           <ButtonSection
+//             text="Main Page"
+//             to="/"
+//             backgroundColor="transparent"
+//           />
+//           <ButtonSection
+//             text="Categories"
+//             to="/categories"
+//             backgroundColor="transparent"
+//           />
+//           <ButtonSection
+//             text="Tools and equipment"
+//             to={`/category/${categoryId}`}
+//             backgroundColor="transparent"
+//           />
+//         </div>
+//         <h2>Tools and equipment</h2>
+
+//         <div className={styles.sorted_section}>
+//           <SortFilteredComponents />
+//         </div>
+//         <div className={styles.products}>
+//           {filteredProducts.map((product) => (
+//             <Link key={product.id} to={`/product/${product.id}`} className={styles.product_link}>
+//               <CategoryProductsCard product={product} />
+//             </Link>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CategoryProductsPage;
+
+
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchProductsByCategory } from "../../redux/actions/productsActions";
+import { fetchProductsByCategory, setMinPriceFilter, setMaxPriceFilter } from "../../redux/actions/productsActions";
 import styles from "./CategoryProductsPage.module.css";
 import SortFilteredComponents from "../../components/SortFilteredComponents";
 import ButtonSection from "../../components/UI/ButtonSection";
@@ -14,10 +92,28 @@ const CategoryProductsPage = () => {
   const { products, loading, error, filters } = useSelector(
     (state) => state.products
   );
+  const [showDiscounted, setShowDiscounted] = useState(false);
+  const [sortBy, setSortBy] = useState('default');
 
   useEffect(() => {
     dispatch(fetchProductsByCategory(categoryId));
   }, [dispatch, categoryId]);
+
+  const handleMinPriceChange = (minPrice) => {
+    dispatch(setMinPriceFilter(minPrice));
+  };
+
+  const handleMaxPriceChange = (maxPrice) => {
+    dispatch(setMaxPriceFilter(maxPrice));
+  };
+
+  const handleSortChange = (selectedValue) => {
+    setSortBy(selectedValue);
+  };
+
+  const handleDiscountChange = (isDiscounted) => {
+    setShowDiscounted(isDiscounted);
+  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -31,11 +127,28 @@ const CategoryProductsPage = () => {
     return <div>No products available</div>;
   }
 
-  const filteredProducts = products.filter((product) => {
-    return (
-      product.price >= filters.minPrice && product.price <= filters.maxPrice
-    );
+  const filteredProducts = products.filter(product => {
+    const price = product.discont_price ?? product.price;
+    const meetsPriceCriteria = price >= filters.minPrice && price <= filters.maxPrice;
+    const meetsDiscountCriteria = showDiscounted ? product.discont_price : true;
+    return meetsPriceCriteria && meetsDiscountCriteria;
   });
+
+  const sortedProducts = [...filteredProducts];
+
+  switch (sortBy) {
+    case 'newest':
+      sortedProducts.sort((a, b) => b.id - a.id);
+      break;
+    case 'price-high-low':
+      sortedProducts.sort((a, b) => (b.discont_price ?? b.price) - (a.discont_price ?? a.price));
+      break;
+    case 'price-low-high':
+      sortedProducts.sort((a, b) => (a.discont_price ?? a.price) - (b.discont_price ?? b.price));
+      break;
+    default:
+      break;
+  }
 
   return (
     <div className="container">
@@ -60,10 +173,16 @@ const CategoryProductsPage = () => {
         <h2>Tools and equipment</h2>
 
         <div className={styles.sorted_section}>
-          <SortFilteredComponents />
+          <SortFilteredComponents
+            onMinPriceChange={handleMinPriceChange}
+            onMaxPriceChange={handleMaxPriceChange}
+            onDiscountChange={handleDiscountChange}
+            onSortChange={handleSortChange}
+            isDiscounted={showDiscounted}
+          />
         </div>
         <div className={styles.products}>
-          {filteredProducts.map((product) => (
+          {sortedProducts.map((product) => (
             <Link key={product.id} to={`/product/${product.id}`} className={styles.product_link}>
               <CategoryProductsCard product={product} />
             </Link>
