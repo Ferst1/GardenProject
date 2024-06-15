3b0a0e5aaf94cdda89f93b9d15b9c9509fabff2a
ghibnerelvira <145448919+ghibnerelvira@users.noreply.github.com>
2024-06-15 22:31:13 +0200

    update Clobal Style, update IconsHeader, update Form 5% and update Style SortedFilteredComponents

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,169 +1,4 @@
 
-
-
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-// import { useSelector } from 'react-redux';
-// import Navigation from "./Navigation";
-// import s from "./Header.module.css";
-// import Logo from "./Logo";
-// import Switcher from "./Switcher";
-// import Favorite from "./Favorite";
-// import Basket from "../Header/Basket";
-// import BurgerMenu from "./BurgerMenu";
-// import MobileMenu from "../MobileMenu";
-
-// const Header = () => {
-//   const [isDarkMode, setIsDarkMode] = useState(false);
-//   const [isMenuOpen, setIsMenuOpen] = useState(false);
-//   const favorites = useSelector((state) => state.products.favorites || []);
-//   const basket = useSelector((state) => state.basket.basket || []);
-
-//   const handleToggleMenu = () => {
-//     setIsMenuOpen(!isMenuOpen);
-//   };
-
-//   return (
-//     <div className={`${s.header} container`}>
-//       <div className={s.logoAndSwitcher}>
-//         <Logo />
-//         <Switcher isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-//       </div>
-//       <Navigation />
-//       <div className={s.favoriteAndBasket}>
-//         <Link to="/favorites" className={s.favoriteLink}>
-//           <Favorite isDarkMode={isDarkMode} />
-//           {favorites.length > 0 && (
-//             <span className={s.favoriteCount}>{favorites.length}</span>
-//           )}
-//         </Link>
-//         <Link to="/basket" className={s.basketLink}>
-//           <div className={s.basketContainer}>
-//             <Basket isInBasket={basket.length > 0} />
-//             {basket.length > 0 && (
-//               <span className={s.basketCount}>{basket.length}</span>
-//             )}
-//           </div>
-//         </Link>
-//         <BurgerMenu onClick={handleToggleMenu} />
-//       </div>
-//       <MobileMenu isMenuOpen={isMenuOpen} handleToggleMenu={handleToggleMenu} />
-//     </div>
-//   );
-// };
-
-// export default Header;
-
-
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-// import { useSelector } from 'react-redux';
-// import Navigation from "./Navigation";
-// import s from "./Header.module.css";
-// import Logo from "./Logo";
-// import Switcher from "./Switcher";
-// import Favorite from "./Favorite";
-// import Basket from "../Header/Basket";
-// import BurgerMenu from "./BurgerMenu";
-// import MobileMenu from "../MobileMenu";
-
-// const Header = () => {
-//   const [isDarkMode, setIsDarkMode] = useState(false);
-//   const [isMenuOpen, setIsMenuOpen] = useState(false);
-//   const favorites = useSelector((state) => state.products.favorites || []);
-//   const basket = useSelector((state) => state.basket.basket || []);
-
-//   const handleToggleMenu = () => {
-//     setIsMenuOpen(!isMenuOpen);
-//   };
-
-//   return (
-//     <div className={`${s.header} container`}>
-//       <div className={s.logoAndSwitcher}>
-//         <Logo />
-//         <Switcher isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-//       </div>
-//       <Navigation />
-//       <div className={s.favoriteAndBasket}>
-//         <Link to="/favorites" className={s.favoriteLink}>
-//           <Favorite isDarkMode={isDarkMode} />
-//           {favorites.length > 0 && (
-//             <span className={s.favoriteCount}>{favorites.length}</span>
-//           )}
-//         </Link>
-//         <Link to="/basket" className={s.basketLink}>
-//           <div className={s.basketContainer}>
-//             <Basket isInBasket={basket.length > 0} />
-//             {basket.length > 0 && (
-//               <span className={s.basketCount}>{basket.length}</span>
-//             )}
-//           </div>
-//         </Link>
-//         <BurgerMenu onClick={handleToggleMenu} />
-//       </div>
-//       <MobileMenu isMenuOpen={isMenuOpen} handleToggleMenu={handleToggleMenu} />
-//     </div>
-//   );
-// };
-
-// export default Header;
-
-
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-// import { useSelector } from 'react-redux';
-// import Navigation from "./Navigation";
-// import s from "./Header.module.css";
-// import Logo from "./Logo";
-// import Switcher from "./Switcher";
-// import Favorite from "./Favorite";
-// import Basket from "../Header/Basket";
-// import BurgerMenu from "./BurgerMenu";
-// import MobileMenu from "../MobileMenu";
-
-// const Header = () => {
-//   const [isDarkMode, setIsDarkMode] = useState(false);
-//   const [isMenuOpen, setIsMenuOpen] = useState(false);
-//   const favorites = useSelector((state) => state.products.favorites || []);
-//   const basket = useSelector((state) => state.basket.basket || []);
-
-//   const handleToggleMenu = () => {
-//     setIsMenuOpen(!isMenuOpen);
-//   };
-
-//   return (
-//     <div className={`${s.header} container`}>
-//       <div className={s.logoAndSwitcher}>
-//         <Logo />
-//         <Switcher isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-//       </div>
-//       <Navigation />
-//       <div className={s.favoriteAndBasket}>
-//         <Link to="/favorites" className={s.favoriteLink}>
-//           <Favorite isDarkMode={isDarkMode} />
-//           {favorites.length > 0 && (
-//             <span className={s.favoriteCount}>{favorites.length}</span>
-//           )}
-//         </Link>
-//         <Link to="/basket" className={s.basketLink}>
-//           <div className={s.basketContainer}>
-//             <Basket isInBasket={basket.length > 0} />
-//             {basket.length > 0 && (
-//               <span className={s.basketCount}>{basket.length}</span>
-//             )}
-//           </div>
-//         </Link>
-//         <BurgerMenu onClick={handleToggleMenu} />
-//       </div>
-//       <MobileMenu isMenuOpen={isMenuOpen} handleToggleMenu={handleToggleMenu} />
-//     </div>
-//   );
-// };
-
-// export default Header;
-
-
-
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from 'react-redux';
@@ -171,8 +6,8 @@ import Navigation from "./Navigation";
 import s from "./Header.module.css";
 import Logo from "./Logo";
 import Switcher from "./Switcher";
-import Favorite from "./Favorite";
-import Basket from "../Header/Basket";
+import Favorite from "../UI/FavoriteHeader";
+import Basket from "../UI/BasketHeader";
 import BurgerMenu from "./BurgerMenu";
 import MobileMenu from "../MobileMenu";
 
@@ -186,10 +21,9 @@ const Header = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const handleBasketClick = () => {
-    // Обработка клика по корзине, например, можно открыть страницу корзины
-    window.location.href = "/basket";
-  };
+  // const handleBasketClick = () => {
+  //   window.location.href = "/basket";
+  // };
 
   return (
     <div className={`${s.header} container`}>
@@ -206,10 +40,12 @@ const Header = () => {
           )}
         </Link>
         <div className={s.basketContainer} >
-          <Basket isDarkMode={isDarkMode} hasItems={basket.length > 0} onClick={handleBasketClick} />
+          <Link to="/basket">
+          <Basket isDarkMode={isDarkMode} hasItems={basket.length > 0}/>
           {basket.length > 0 && (
             <span className={s.basketCount}>{basket.length}</span>
           )}
+          </Link>
         </div>
         <BurgerMenu onClick={handleToggleMenu} />
       </div>
