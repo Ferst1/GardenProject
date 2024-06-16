ff6a463dc45fe425d745c1645ce78a288186b6bd
Ferst1 <36508011+Ferst1@users.noreply.github.com>
2024-06-16 18:41:48 +0200

    basket
    basket updated

--- src/components/UI/ButtonAddToCard/index.jsx
+++ src/components/UI/ButtonAddToCard/index.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import s from './ButtonAddToCard.module.css';
 import { addToBasket } from '../../../redux/basketReducer';
 
-const ButtonAddToCard = ({ product, onAddToBasket }) => {
+const ButtonAddToCard = ({ product, onAddToBasket,className }) => {
   const dispatch = useDispatch();
   const basket = useSelector((state) => state.basket.basket);
   const [isAdded, setIsAdded] = useState(false);
