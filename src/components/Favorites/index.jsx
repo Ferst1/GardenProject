4c800fa7330a9e452b1d9547998dd2b7f91fd1c6
Ferst1 <36508011+Ferst1@users.noreply.github.com>
2024-06-23 21:03:33 +0200

    favotitepage lear button return to shop

--- src/components/Favorites/index.jsx
+++ src/components/Favorites/index.jsx
@@ -1,19 +1,34 @@
 
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import ProductsCard from '../ProductsCard'; 
+import Button from '../UI/Button';
 import styles from './Favorites.module.css';
 
 const Favorites = () => {
   const favorites = useSelector((state) => state.products.favorites || []);
+  const navigate = useNavigate();
+
+  const handleContinueShopping = () => {
+    navigate('/all_products');
+  };
 
   if (favorites.length === 0) {
-    return <div className={styles.empty}>No favorite products</div>;
+    return (
+      <div className={styles.empty}>
+        <p>Looks like you have no items in your favorites currently.</p>
+        <Button
+          text="Continue Shopping"
+          onClick={handleContinueShopping}
+          className={styles.button_details}
+        />
+      </div>
+    );
   }
 
   return (
     <div className={styles.favorites}>
-      
       <div className={styles.favoritesList}>
         {favorites.map((product) => (
           <ProductsCard key={product.id} product={product} />
