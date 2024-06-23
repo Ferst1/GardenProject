c075fa31b1637d7a27e79137788f5b7c6ad7b99d
Ferst1 <36508011+Ferst1@users.noreply.github.com>
2024-06-23 19:20:18 +0200

    updated

--- src/components/AllProducts/index.jsx
+++ src/components/AllProducts/index.jsx
@@ -1,4 +1,28 @@
 
+// import React from 'react';
+// import s from './AllProducts.module.css';
+// import ProductsCard from '../ProductsCard';
+// import CardSkeleton from '../CardSkeleton/CardSkeleton'; 
+
+// const AllProducts = ({ products, loading }) => {
+//   return (
+//     <div className="container">
+//       <div className={s.products_grid}>
+//         {loading 
+//           ? Array(11).fill().map((_, index) => (
+//               <CardSkeleton key={index} />
+//             ))
+//           : products.map((product) => (
+//               <ProductsCard key={product.id} product={product} />
+//             ))
+//         }
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AllProducts;
+
 import React from 'react';
 import s from './AllProducts.module.css';
 import ProductsCard from '../ProductsCard';
@@ -8,18 +32,20 @@ const AllProducts = ({ products, loading }) => {
   return (
     <div className="container">
       <div className={s.products_grid}>
-        {loading 
-          ? Array(10).fill().map((_, index) => (
-              <CardSkeleton key={index} />
+        {loading
+          ? Array(11).fill().map((_, index) => (
+              <div key={index} className={s.card}>
+                <CardSkeleton />
+              </div>
             ))
           : products.map((product) => (
-              <ProductsCard key={product.id} product={product} />
-            ))
-        }
+              <div key={product.id} className={s.card}>
+                <ProductsCard product={product} />
+              </div>
+            ))}
       </div>
     </div>
   );
 };
 
 export default AllProducts;
-
--- src/components/CardSkeleton/CardSkeleton.jsx
+++ src/components/CardSkeleton/CardSkeleton.jsx
@@ -1,5 +1,5 @@
 import Skeleton from "react-loading-skeleton"
-
+import 'react-loading-skeleton/dist/skeleton.css';
 import styled from "./CardSkeleton.module.css"
 
 const CardSkeleton = () => {
