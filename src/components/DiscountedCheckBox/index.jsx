cc7b09142db1776cf1ce22def64eb81e0bf03f09
Ferst1 <36508011+Ferst1@users.noreply.github.com>
2024-06-27 20:16:24 +0200

    checkbox updated reducer action

--- src/components/DiscountedCheckBox/index.jsx
+++ src/components/DiscountedCheckBox/index.jsx
@@ -1,4 +1,31 @@
 
+// import React from 'react';
+// import defaultStyles from './DiscountedCheckBox.module.css';
+// import checkedIcon from '../../media/icons/check box=active.svg'; 
+// import uncheckedIcon from '../../media/icons/check box=normal.svg';
+
+// const DiscountedCheckBox = ({ styles = defaultStyles, setShowDiscounted, showDiscounted }) => {
+//   const handleCheckboxClick = () => {
+//     setShowDiscounted(!showDiscounted);
+//   };
+
+//   return (
+//     <div className={styles.discount_wrapper}>
+//       <label htmlFor="discounted"><h3>Discounted items</h3></label>
+//       <div className={styles.checkbox} onClick={handleCheckboxClick}>
+//         <img
+//           src={showDiscounted ? checkedIcon : uncheckedIcon} 
+//           className={styles.checkbox_image}
+//              alt="Discount checkbox"
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default DiscountedCheckBox;
+
+
 import React from 'react';
 import defaultStyles from './DiscountedCheckBox.module.css';
 import checkedIcon from '../../media/icons/check box=active.svg'; 
@@ -16,7 +43,7 @@ const DiscountedCheckBox = ({ styles = defaultStyles, setShowDiscounted, showDis
         <img
           src={showDiscounted ? checkedIcon : uncheckedIcon} 
           className={styles.checkbox_image}
-             alt="Discount checkbox"
+          alt="Discount checkbox"
         />
       </div>
     </div>
--- src/pages/CategoryProductsPage/index.jsx
+++ src/pages/CategoryProductsPage/index.jsx
@@ -1,23 +1,103 @@
 
-import React, { useState, useEffect } from "react";
+
+// import React, { useEffect } from "react";
+// import { Link, useParams } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchProductsByCategory, setMinPriceFilter, setMaxPriceFilter, setShowDiscounted, setSortBy, filterAndSortProducts } from "../../redux/actions/productsActions";
+// import styles from "./CategoryProductsPage.module.css";
+// import SortFilteredComponents from "../../components/SortFilteredComponents";
+// import ButtonSection from "../../components/UI/ButtonSection";
+// import CategoryProductsCard from "../../components/CategoryProductsCard";
+// import CardSkeleton from "../../components/CardSkeleton/CardSkeleton";
+
+// const CategoryProductsPage = () => {
+//   const { categoryId } = useParams();
+//   const dispatch = useDispatch();
+//   const { filteredAndSortedProducts, loading, error, filters, showDiscounted, sortBy } = useSelector(
+//     (state) => state.products
+//   );
+
+//   useEffect(() => {
+//     dispatch(fetchProductsByCategory(categoryId));
+//   }, [dispatch, categoryId]);
+
+//   const handleMinPriceChange = (minPrice) => {
+//     dispatch(setMinPriceFilter(minPrice));
+//     dispatch(filterAndSortProducts());
+//   };
+
+//   const handleMaxPriceChange = (maxPrice) => {
+//     dispatch(setMaxPriceFilter(maxPrice));
+//     dispatch(filterAndSortProducts());
+//   };
+
+//   const handleSortChange = (selectedValue) => {
+//     dispatch(setSortBy(selectedValue));
+//     dispatch(filterAndSortProducts());
+//   };
+
+//   const handleDiscountChange = (isDiscounted) => {
+//     dispatch(setShowDiscounted(isDiscounted));
+//     dispatch(filterAndSortProducts());
+//   };
+
+//   return (
+//     <div className="container">
+//       <div>
+//         <div className={styles.buttons_wrapper}>
+//           <ButtonSection text="Main Page" to="/" />
+//           <ButtonSection text="Categories" to="/categories"  />
+//           <ButtonSection text="Tools and equipment" to={`/category/${categoryId}`} />
+//         </div>
+//         <h2>Tools and equipment</h2>
+//         <div className={styles.sorted_section}>
+//           <SortFilteredComponents
+//             onMinPriceChange={handleMinPriceChange}
+//             onMaxPriceChange={handleMaxPriceChange}
+//             onDiscountChange={handleDiscountChange}
+//             onSortChange={handleSortChange}
+//             isDiscounted={showDiscounted}
+//           />
+//         </div>
+//         <div className={styles.products}>
+//           {loading ? (
+//             [...Array(10)].map((_, index) => <CardSkeleton key={index} />)
+//           ) : error ? (
+//             <div className="container">Error: {error}</div>
+//           ) : !Array.isArray(filteredAndSortedProducts) || filteredAndSortedProducts.length === 0 ? (
+//             <div className="container">No products available</div>
+//           ) : (
+//             filteredAndSortedProducts.map((product) => (
+//               <Link key={product.id} to={`/product/${product.id}`} className={styles.product_link}>
+//                 <CategoryProductsCard product={product} />
+//               </Link>
+//             ))
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CategoryProductsPage;
+
+
+import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchProductsByCategory, setMinPriceFilter, setMaxPriceFilter } from "../../redux/actions/productsActions";
+import { fetchProductsByCategory, setMinPriceFilter, setMaxPriceFilter, setShowDiscounted, setSortBy, filterAndSortProducts } from "../../redux/actions/productsActions";
 import styles from "./CategoryProductsPage.module.css";
 import SortFilteredComponents from "../../components/SortFilteredComponents";
 import ButtonSection from "../../components/UI/ButtonSection";
 import CategoryProductsCard from "../../components/CategoryProductsCard";
-import { sortProducts } from "../../utils";
 import CardSkeleton from "../../components/CardSkeleton/CardSkeleton";
 
 const CategoryProductsPage = () => {
   const { categoryId } = useParams();
   const dispatch = useDispatch();
-  const { products, loading, error, filters } = useSelector(
+  const { filteredAndSortedProducts, loading, error, filters, showDiscounted, sortBy } = useSelector(
     (state) => state.products
   );
-  const [showDiscounted, setShowDiscounted] = useState(false);
-  const [sortBy, setSortBy] = useState('default');
 
   useEffect(() => {
     dispatch(fetchProductsByCategory(categoryId));
@@ -25,51 +105,33 @@ const CategoryProductsPage = () => {
 
   const handleMinPriceChange = (minPrice) => {
     dispatch(setMinPriceFilter(minPrice));
+    dispatch(filterAndSortProducts());
   };
 
   const handleMaxPriceChange = (maxPrice) => {
     dispatch(setMaxPriceFilter(maxPrice));
+    dispatch(filterAndSortProducts());
   };
 
   const handleSortChange = (selectedValue) => {
-    setSortBy(selectedValue);
+    dispatch(setSortBy(selectedValue));
+    dispatch(filterAndSortProducts());
   };
 
   const handleDiscountChange = (isDiscounted) => {
-    setShowDiscounted(isDiscounted);
+    dispatch(setShowDiscounted(isDiscounted));
+    dispatch(filterAndSortProducts());
   };
-
-  const filteredProducts = products.filter(product => {
-    const price = product.discont_price ?? product.price;
-    const meetsPriceCriteria = price >= filters.minPrice && price <= filters.maxPrice;
-    const meetsDiscountCriteria = showDiscounted ? product.discont_price : true;
-    return meetsPriceCriteria && meetsDiscountCriteria;
-  });
-
-  const sortedProducts = sortProducts(filteredProducts, sortBy);
 
   return (
     <div className="container">
       <div>
         <div className={styles.buttons_wrapper}>
-          <ButtonSection
-            text="Main Page"
-            to="/"
-            backgroundColor="transparent"
-          />
-          <ButtonSection
-            text="Categories"
-            to="/categories"
-            backgroundColor="transparent"
-          />
-          <ButtonSection
-            text="Tools and equipment"
-            to={`/category/${categoryId}`}
-            backgroundColor="transparent"
-          />
+          <ButtonSection text="Main Page" to="/" backgroundColor="transparent" />
+          <ButtonSection text="Categories" to="/categories" backgroundColor="transparent" />
+          <ButtonSection text="Tools and equipment" to={`/category/${categoryId}`} backgroundColor="transparent" />
         </div>
         <h2>Tools and equipment</h2>
-
         <div className={styles.sorted_section}>
           <SortFilteredComponents
             onMinPriceChange={handleMinPriceChange}
@@ -81,15 +143,13 @@ const CategoryProductsPage = () => {
         </div>
         <div className={styles.products}>
           {loading ? (
-            [...Array(10)].map((_, index) => (
-              <CardSkeleton key={index} />
-            ))
+            [...Array(10)].map((_, index) => <CardSkeleton key={index} />)
           ) : error ? (
             <div className="container">Error: {error}</div>
-          ) : !Array.isArray(products) || products.length === 0 ? (
+          ) : !Array.isArray(filteredAndSortedProducts) || filteredAndSortedProducts.length === 0 ? (
             <div className="container">No products available</div>
           ) : (
-            sortedProducts.map((product) => (
+            filteredAndSortedProducts.map((product) => (
               <Link key={product.id} to={`/product/${product.id}`} className={styles.product_link}>
                 <CategoryProductsCard product={product} />
               </Link>
