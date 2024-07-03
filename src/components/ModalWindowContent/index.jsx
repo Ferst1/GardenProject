472a9f8d1bdfef06c8681032ee255eb02594f090
ghibnerelvira <145448919+ghibnerelvira@users.noreply.github.com>
2024-07-03 23:49:55 +0200

    POST request Order and Discount, styling OrderModalWindow, styling BasketProductPage + media query, logic

--- src/components/ModalWindowContent/index.jsx
+++ src/components/ModalWindowContent/index.jsx
@@ -4,7 +4,7 @@ const ModalWindowContent = () => {
   return (
     <div className={styled.modal_content}>
         <h3>Congratulations!</h3>
-      <p>Congratulations! Your order has been successfully placed on the website.</p>
+      <p>Your order has been successfully placed on the website.</p>
       <p>A manager will contact you shortly to confirm your order.</p>
     </div>
     )
